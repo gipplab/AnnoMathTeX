cbf94238f43d6ca425a8930b722508aca5eaf195
ian <ian.mackerracher@uni-konstanz.de>
2019-01-21 12:02:49 +0100

    Package for sparql queries

--- sparql_crawler.py
+++ sparql_crawler.py
@@ -1,2 +1,18 @@
-import requests
+from SPARQLWrapper import SPARQLWrapper, JSON
+from sparql_queries.mathematical_expression import mathematical_expression_query
+import pandas as pd
+
+
+sparql = SPARQLWrapper("https://query.wikidata.org/sparql")
+
+sparql.setQuery(mathematical_expression_query)
+
+
+
+sparql.setReturnFormat(JSON)
+results = sparql.query().convert()
+
+results_df = pd.io.json.json_normalize(results['results']['bindings'])
+print(results_df[['item.value', 'itemLabel.value']].head())
+
 
---
+++ sparql_queries/mathematical_expression.py
@@ -0,0 +1,8 @@
+mathematical_expression_query = """
+SELECT 
+?item ?itemLabel 
+WHERE {
+  ?item wdt:P2534 ?mathematical_expression;
+  SERVICE wikibase:label { bd:serviceParam wikibase:language "[AUTO_LANGUAGE],en". }
+}
+"""
