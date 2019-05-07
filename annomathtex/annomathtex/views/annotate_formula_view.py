e3fb78f36d1998f2a6f3cd1b81476e9c02679828
ian <ian.mackerracher@uni-konstanz.de>
2019-05-07 09:15:16 +0100

    cache

--- annomathtex/annomathtex/views/annotate_formula_view.py
+++ annomathtex/annomathtex/views/annotate_formula_view.py
@@ -160,6 +160,8 @@ class FileUploadView(View):
         """
         path = view_cache_path + 'dicts'
 
+        print(dicts)
+
         with open(path, 'wb') as outfile:
             pickle.dump(dicts, outfile)
 
