6447f417ccdcd7a7dcae0a2eda2e70ea19516086
ian <ian.mackerracher@uni-konstanz.de>
2019-09-24 15:09:02 +0200

    Code cleanup: session saved in saparte class

--- annomathtex/annomathtex/views/annotate_formula_view.py
+++ annomathtex/annomathtex/views/annotate_formula_view.py
@@ -20,6 +20,7 @@ from .helper_classes.cache_handler import CacheHandler
 from .helper_classes.wikipedia_query_handler import WikipediaQueryHandler
 from .helper_classes.wikipedia_article_name_handler import WikipediaArticleNameHandler
 from .helper_classes.repo_content_handler import RepoContentHandler
+from .helper_classes.session_saved_handler import SessionSavedHandler
 
 
 logging.basicConfig(level=logging.INFO)
@@ -33,69 +34,7 @@ class FileUploadView(View):
     form_class = UploadFileForm
     initial = {'key': 'value'}
     save_annotation_form = {'form': SaveAnnotationForm()}
-    #template_name = 'file_upload_wiki_suggestions_2.html'
     template_name = 'annotation_template_tmp.html'
-
-
-    def handle_marked(self, request):
-        """
-        This method receives data from the frontend, when the user mouse clicks the "save" button.
-
-        Annotated items: e.g. the user annotated the identifier "E" with the wikidata item "energy (Q11379)".
-        Marked items: A word that wasn't found by the named entity tagger, but the user decided it should have been.
-        Unmarked items: A Word that was found by the named entity tagger, but the user decided it shouldn't have been.
-
-        :param request: Request object. Request made by the user through the frontend.
-        :return: The rendered response containing the template name and the necessary form.
-        """
-        print('IN HANDLE MARKED')
-        items = {k: jquery_unparam(v) for (k, v) in request.POST.items()}
-        annotations = items['annotations']
-        file_name = items['fileName']['f']
-        manual_recommendations = items['manualRecommendations']
-
-
-        m = ManualRecommendationsCleaner(manual_recommendations)
-        cleaned_manual_recommendations = m.get_recommendations()
-
-
-        new_annotations = handle_annotations(annotations)
-
-        annotation_file_path = create_annotation_file_path(file_name)
-        with open(annotation_file_path, 'w') as f:
-            __LOGGER__.debug(' WRITING TO FILE {}'.format(annotation_file_path))
-            json.dump(new_annotations, f)
-
-
-        eval_file_writer = EvalFileWriter(new_annotations, file_name)
-        #eval_file_writer.write()
-        evaluation_csv_string = eval_file_writer.get_csv_for_repo()
-        data_repo_handler = DataRepoHandler()
-        #file_name = re.sub(r'\..*', '.csv', file_name)
-
-        annotation_file_name = create_annotation_file_name(file_name)
-        evaluation_file_name = create_evaluation_file_name(file_name)
-        data_repo_handler.commit_manual_recommendations(cleaned_manual_recommendations)
-        data_repo_handler.commit_formula_concepts(annotations)
-        data_repo_handler.commit_annotations(annotation_file_name, json.dumps(annotations))
-        data_repo_handler.commit_evaluation(evaluation_file_name, evaluation_csv_string)
-
-        return HttpResponse(
-            json.dumps({'testkey': 'testvalue'}),
-            content_type='application/json'
-        )
-
-
-    def get_repo_content(self):
-        """
-        Get the repo content for the datarepo/annotation folder, i.e. all files that have been annotated in the past.
-        :return:
-        """
-        file_names = DataRepoHandler().list_directory()
-        return HttpResponse(
-                            json.dumps({'fileNames': file_names}),
-                            content_type='application/json'
-        )
 
 
     def get(self, request, *args, **kwargs):
@@ -131,16 +70,14 @@ class FileUploadView(View):
 
         elif 'annotations' in request.POST:
             print('annotations')
-            return self.handle_marked(request)
+            return SessionSavedHandler(request).save()
 
-        #todo: remove this method from this view (and others that aren't used)
         #todo: check if not used
         elif 'wikipediaSubmit' in request.POST:
             print("WIKIPEDIA SUBMIT")
             return WikipediaQueryHandler(request).get_suggestions()
 
         elif 'wikipediaArticleName' in request.POST:
-            #todo: put in separate method (consistency)
             print('wikipediaArticleName')
             return WikipediaArticleNameHandler(request).handle_name()
 
---
+++ annomathtex/annomathtex/views/helper_classes/session_saved_handler.py
@@ -0,0 +1,72 @@
+import json
+from jquery_unparam import jquery_unparam
+from django.http import HttpResponse
+from ...views.data_repo_handler import DataRepoHandler, ManualRecommendationsCleaner
+from ...views.helper_functions import handle_annotations
+from ...views.eval_file_writer import EvalFileWriter
+
+from ...config import *
+
+class SessionSavedHandler:
+    """
+    This class receives data from the frontend, when the user mouse clicks the "save" button.
+
+    Annotated items: e.g. the user annotated the identifier "E" with the wikidata item "energy (Q11379)".
+    Marked items: A word that wasn't found by the named entity tagger, but the user decided it should have been.
+    Unmarked items: A Word that was found by the named entity tagger, but the user decided it shouldn't have been.
+
+    :param request: Request object. Request made by the user through the frontend.
+    :return: The rendered response containing the template name and the necessary form.
+    """
+
+
+    #NOTE: in earlier version, annotations was used instead of cleaned_annotations, I think this was a mistake
+    #      but haven't tested yet
+
+
+    def __init__(self, request):
+        self.request = request
+
+
+
+    def save(self):
+        items = {k: jquery_unparam(v) for (k, v) in self.request.POST.items()}
+        annotations = items['annotations']
+        file_name = items['fileName']['f']
+        manual_recommendations = items['manualRecommendations']
+
+        m = ManualRecommendationsCleaner(manual_recommendations)
+        cleaned_manual_recommendations = m.get_recommendations()
+        cleaned_annotations = handle_annotations(annotations)
+
+        #self.save_files_locally(file_name, cleaned_annotations)
+        #self.save_files_to_repo(file_name, cleaned_annotations, cleaned_manual_recommendations)
+
+        return HttpResponse(
+            json.dumps({'testkey': 'testvalue'}),
+            content_type='application/json'
+        )
+
+
+    def save_files_locally(self, file_name, cleaned_annotations):
+        annotation_file_path = create_annotation_file_path(file_name)
+        with open(annotation_file_path, 'w') as f:
+            #__LOGGER__.debug(' WRITING TO FILE {}'.format(annotation_file_path))
+            json.dump(cleaned_annotations, f)
+
+
+
+    def save_files_to_repo(self, file_name, cleaned_annotations, cleaned_manual_recommendations):
+
+
+        data_repo_handler = DataRepoHandler()
+        data_repo_handler.commit_manual_recommendations(cleaned_manual_recommendations)
+        data_repo_handler.commit_formula_concepts(cleaned_annotations)
+        annotation_file_name = create_annotation_file_name(file_name)
+        data_repo_handler.commit_annotations(annotation_file_name, json.dumps(cleaned_annotations))
+        eval_file_writer = EvalFileWriter(cleaned_annotations, file_name)
+        evaluation_csv_string = eval_file_writer.get_csv_for_repo()
+        evaluation_file_name = create_evaluation_file_name(file_name)
+        data_repo_handler.commit_evaluation(evaluation_file_name, evaluation_csv_string)
+        return
+
