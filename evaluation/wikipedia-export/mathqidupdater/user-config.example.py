b29acb02834b41ab7b989a82c787e689129e0194
Moritz Schubotz (physikerwelt) <wiki@physikerwelt.de>
2020-12-02 17:40:06 +0100

    Document how to obtain oauth tokens

--- evaluation/wikipedia-export/mathqidupdater/user-config.example.py
+++ evaluation/wikipedia-export/mathqidupdater/user-config.example.py
@@ -1,3 +1,6 @@
 family = 'wikipedia'
 mylang = 'en'
 usernames['wikipedia']['en'] = 'ZentralBot'
+# Follow the manual at https://www.mediawiki.org/wiki/Manual:Pywikibot/OAuth/Wikimedia to replace XXX with actual values
+authenticate['*.wikipedia.org'] = ('XXX', 'XXX',
+                                   'XXX', 'XXX')
