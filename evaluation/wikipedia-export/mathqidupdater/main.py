66d8f69c6715a2a6644a76caa189a644e8d55fd2
Moritz Schubotz (physikerwelt) <wiki@physikerwelt.de>
2020-12-03 07:28:51 +0100

    Add testfile

--- evaluation/wikipedia-export/mathqidupdater/main.py
+++ evaluation/wikipedia-export/mathqidupdater/main.py
@@ -1,11 +1,14 @@
 import pywikibot
 
 site = pywikibot.Site()
-page = pywikibot.Page(site, u"User:ZentralBot")
+page = pywikibot.Page(site, 'Harmonic_oscillator')
 text = page.text
-formula = 'E=mc^2'
-qid = 35875
-search = f'<math>{formula}</math>'
-replace = f'<math qid=Q{qid}>{formula}</math>'
-page.text = text.replace(search, replace, 1)
-page.save(u"Add qID")
+fh =  open('../tests/data/Harmonic_oscillator.txt', 'w',encoding='utf-8')
+fh.write(text)
+fh.close()
+# formula = 'E=mc^2'
+# qid = 35875
+# search = f'<math>{formula}</math>'
+# replace = f'<math qid=Q{qid}>{formula}</math>'
+# page.text = text.replace(search, replace, 1)
+# page.save(u"Add qID")
