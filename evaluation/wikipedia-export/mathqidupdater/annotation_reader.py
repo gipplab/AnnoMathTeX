c007f9324ea3b35e9cce3e78ef8c8e28a37277d1
Moritz Schubotz (physikerwelt) <wiki@physikerwelt.de>
2020-12-03 07:18:35 +0100

    Find relevant entries

--- evaluation/wikipedia-export/mathqidupdater/annotation_reader.py
+++ evaluation/wikipedia-export/mathqidupdater/annotation_reader.py
@@ -1,7 +1,18 @@
-import os
+import os, csv, re
 
 
 def get_file_list(location):
     return [f for f in os.listdir(location) if f.endswith('.csv')]
 
 
+def get_qids(filename):
+    pat_q = re.compile('\(Q(\d+)\)$')
+    result = []
+    with open(filename) as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            content = row['Identifier / Formula']
+            m = pat_q.search(row['Name'])
+            if m and content.__contains__('='):
+                result.append([content, m.group(1)])
+        return result
--- evaluation/wikipedia-export/tests/test_annotation_reader.py
+++ evaluation/wikipedia-export/tests/test_annotation_reader.py
@@ -1,4 +1,4 @@
-from mathqidupdater.annotation_reader import get_file_list
+from mathqidupdater.annotation_reader import get_file_list, get_qids
 import unittest
 
 
@@ -6,3 +6,11 @@ class TestAnnotationReader(unittest.TestCase):
     def test_file_list(self):
         files = get_file_list('./data')
         self.assertEqual(files, ['Harmonic_oscillator.csv', 'Jerk_(physics).csv'])
+
+    def test_get_qids(self):
+        qids = get_qids('./data/Harmonic_oscillator.csv')
+        self.assertEqual(qids, [[' \\vec F = -k \\vec x, ', '170282'],
+                                ['F = m a = m \\frac{\\mathrm{d}^2x}{\\mathrm{d}t^2} = m\\ddot{x} = -k x. ',
+                                 '170282'],
+                                [' x(t) = A \\cos(\\omega t + \\varphi), ', '3299367'],
+                                ['\\omega = \\sqrt{\\frac k m}.', '834020']])
